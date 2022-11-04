b5bdbe3e8b5cb44cb5b59f2512115c48bf931038
Tarun Mahajan <2922986+tango4567@users.noreply.github.com>
2022-11-04 06:43:43 +0530

    Added GoogleLogin With Error

---
+++ lib/dashboard_screen.dart
@@ -0,0 +1,24 @@
+// Home page screen
+
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatefulWidget {
+  DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        title: const Text("My Dashboard"),
+      ),
+      body: const Center(child: Text('Change this user name to current user')),
+    );
+  }
+}
